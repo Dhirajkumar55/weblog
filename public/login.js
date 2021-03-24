ebf7480bae84b14e3d298ac967ac6b3acf4bcf13
bhima2001 <56587387+bhima2001@users.noreply.github.com>
2021-03-24 21:07:20 +0530

    Just added a separate page for categories to be redirect when clicked

--- public/login.js
+++ public/login.js
@@ -1,12 +1,6 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-// const x = document.querySelector('.ghost');
-
-// function changeUrl(){
-// 	javascript:window.location.href="/menu/signup";
-// 	container.classList.add("right-panel-active");
-// }
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
